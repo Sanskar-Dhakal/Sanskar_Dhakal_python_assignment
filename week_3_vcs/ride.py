018a2b5256a6b37e9dcaef22d5fc669a9517f591
Sanskar <sanskardhakal68@gmail.com>
2026-07-19 23:42:42 +0545

    changed

--- week_3_vcs/ride.py
+++ week_3_vcs/ride.py
@@ -11,4 +11,12 @@ def estimate_fare(distance_km, vehicle_type, surge=1.0):
         return f"Invalid vehicle type: {vehicle_type}"
 
     fare = base_rate * distance_km * surge
-    return round(fare, 2)
+    return round(fare, 2)  
+print(estimate_fare(10, "bike"))
+print(estimate_fare(10, "auto"))
+print(estimate_fare(10, "car"))
+print(estimate_fare(10, "premium"))  
+print(estimate_fare(10, "bike", 2))   
+print(estimate_fare(10, "auto", 2))    
+print(estimate_fare(10, "car", 2))     
+print(estimate_fare(10, "premium", 2))   
